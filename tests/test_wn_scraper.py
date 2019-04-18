31629de8854e382b432fd3dbb0d45af1b8e19a65
Calvin De Lima <calvindlm@gmail.com>
2019-04-17 20:19:39 -0400

    Added scraper process

--- tests/test_wn_scraper.py
+++ tests/test_wn_scraper.py
@@ -1,21 +1,6 @@
 from datetime import datetime
-from src.wn_scraper.driver import get_driver
-from src.wn_scraper.webpage import get_7_day_forecast_image
-from src.wn_scraper.ocr import get_text
-from src.wn_scraper.data import get_wn_pop_data
+from src.wn_scraper.process import get_forecast_data
 from support.constants import FORECAST_URL, CREDS
-
-def get_forecast_data() -> None:
-  # Get the forecast image
-  image_data = get_7_day_forecast_image(FORECAST_URL)
-
-  # Extract the text
-  text = get_text(CREDS, image_data)
-
-  # Extract the data
-  data = get_wn_pop_data(text)
-
-  return data
 
 def test_run_process():
   data_path = 'data/'
@@ -23,6 +8,6 @@ def test_run_process():
     data_path,
     datetime.now()
   )
-  get_forecast_data().to_csv(file_name, index=False)
+  get_forecast_data(CREDS, FORECAST_URL).to_csv(file_name, index=False)
 
 
